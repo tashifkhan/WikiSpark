55d178c90cf1470240da90705e5dd4a3c1714ec1
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-06-20 14:08:58 +0530

    removed rendency

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { Toaster } from "./components/ui/toaster";
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import WikipediaPage from "./pages/WikipediaPage";
 
 const queryClient = new QueryClient();
+
+// Wrapper to extract :title param and pass as initialArticle
+const WikiRoute = () => {
+	const { title } = useParams();
+	return <Index initialArticle={title} />;
+};
 
 const App = () => (
 	<QueryClientProvider client={queryClient}>
@@ -17,7 +22,7 @@ const App = () => (
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Index />} />
-					<Route path="wiki/:title?" element={<WikipediaPage />} />
+					<Route path="wiki/:title?" element={<WikiRoute />} />
 					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</BrowserRouter>
